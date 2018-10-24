f2142a9f83638032dc0f9fa689f5d3be3400d68b
李鼎 <liding@icourt.cc>
2018-10-24 13:25:08 +0800

    vue项目使用优雅的使用svg示例

---
+++ src/views/svgDemo.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="svg-demo">
+        <ul>
+            <li v-for="(name, index) in svgName" :key="index">
+                <svg-icon :iconClass="name"></svg-icon>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'svgDemo',
+  data () {
+    return {
+      svgName: ['user', 'password', 'email']
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+    .svg-demo{
+        text-align: center;
+        ul, li{
+            list-style: none;
+            padding: 0;
+            margin: 0;
+            line-height: 2;
+        }
+    }
+</style>
---
+++ vue.config.js
@@ -0,0 +1,29 @@
+
+const path = require('path')
+
+function resolve (dir) {
+  return path.join(__dirname, './', dir)
+}
+
+module.exports = {
+  chainWebpack: config => {
+    // svg rule loader
+    const svgRule = config.module.rule('svg') // 找到svg-loader
+    svgRule.uses.clear() // 清除已有的loader, 如果不这样做会添加在此loader之后
+    svgRule.exclude.add(/node_modules/) // 正则匹配排除node_modules目录
+    svgRule // 添加svg新的loader处理
+      .test(/\.svg$/)
+      .use('svg-sprite-loader')
+      .loader('svg-sprite-loader')
+      .options({
+        symbolId: 'icon-[name]'
+      })
+
+    // 修改images loader 添加svg处理
+    const imagesRule = config.module.rule('images')
+    imagesRule.exclude.add(resolve('src/icons'))
+    config.module
+      .rule('images')
+      .test(/\.(png|jpe?g|gif|svg)(\?.*)?$/)
+  }
+}
